8e5678292235d28e83a0a65704f41ac156295d91
Admin <Admin@192.168.2.8>
2018-03-27 21:51:48 +0300

    Add inheritance

---
+++ Inheritance/src/Manager.java
@@ -0,0 +1,31 @@
+
+public class Manager extends Employee {
+	/**
+	 * @param n
+	 *            ≥м€ сп≥вроб≥тника
+	 * @param s
+	 *            зарплата
+	 * @param year
+	 *            р≥к найму
+	 * @param month
+	 *            м≥с€ць найму
+	 * @param day
+	 *            день найму
+	 */
+
+	public Manager(String n, double s, int year, int month, int day) {
+		super(n, s, year, month, day);
+		bonus = 0;
+	}
+
+	public double getSelery() {
+		double baseSalary = super.getSalary();
+		return baseSalary + bonus;
+	}
+
+	public void setBonus(double b) {
+		bonus = b;
+	}
+
+	private double bonus;
+}
---
+++ Inheritance/src/ManagerTest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ManagerTest {
+
+	public static void main(String[] args) {
+		// create a new object
+		Manager boss = new Manager("Petro Tsupryk", 12000, 2016, 10, 1);
+		boss.setBonus(5000);
+
+		Employee[] staff = new Employee[3];
+
+		// filling the array staff of objects Managet and Employee
+		staff[0] = boss;
+		staff[1] = new Employee("Trotsyuk Roman", 10000, 2013, 12, 5);
+		staff[2] = new Employee("Oksana Sheremeta", 4000, 2014, 5, 22);
+
+		// Extract information about all Employee objects
+		for (Employee e : staff)
+			System.out.println("name=" + e.getName() + ", salary= " + e.getSalary());
+	}
+
+}
